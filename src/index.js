84dc515670880ec9e098d6efe586fb3c813b1170
elenabarashkova <lena.barashkova23@gmail.com>
2021-04-21 18:00:01 +0300

    refactor: resolved task

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 
 // You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-if(matrix) {
-    let commonArray = [];
-    for (let i = 0; i < matrix.length; i++) {
-        if(i % 2 !== 0) {
-            let reverseMatrix = matrix[i].reverse();
-            commonArray = commonArray.concat(reverseMatrix);
-            } else {
-            commonArray = commonArray.concat(matrix[i]);
-        }
-    }
-    return commonArray;
-    } else {
-        return [];
-    }
-}
+//
+// module.exports = function towelSort (matrix) {
+// if(matrix) {
+//     let commonArray = [];
+//     for (let i = 0; i < matrix.length; i++) {
+//         if(i % 2 !== 0) {
+//             let reverseMatrix = matrix[i].reverse();
+//             commonArray = commonArray.concat(reverseMatrix);
+//             } else {
+//             commonArray = commonArray.concat(matrix[i]);
+//         }
+//     }
+//     return commonArray;
+//     } else {
+//         return [];
+//     }
+// }
 
 //second variant of solving
 // module.exports = function towelSort (matrix) {
@@ -38,3 +38,12 @@ if(matrix) {
 //   	return [];
 //   }
 // }  
+
+module.exports = function towelSort (matrix) {
+  if(matrix && matrix.length !== 0) {
+    return matrix.map(function(item, index) {
+      return index%2 === 0 ? item : item.reverse();
+    }).flat();
+  }
+  return [];
+}
